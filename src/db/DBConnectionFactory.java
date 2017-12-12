d983836b0157b61c57b7b0a697be34ce245ca858
Yufeng Yuan <Yufeng_Yuan@student.uml.edu>
2017-12-12 14:09:07 -0800

    Add project structure and some source code

---
+++ src/db/DBConnectionFactory.java
@@ -0,0 +1,27 @@
+package db;
+
+import db.mongodb.*;
+import db.mysql.*;
+
+public class DBConnectionFactory {
+	// This should change based on the pipeline.
+	private static final String DEFAULT_DB = "mysql";
+
+	// Create a DBConnection based on given db type.
+	public static DBConnection getDBConnection(String db) {
+		switch (db) {
+		case "mysql":
+			return MySQLConnection.getInstance();
+		case "mongodb":
+			return MongoDBConnection.getInstance();
+		// You may try other dbs and add them here.
+		default:
+			throw new IllegalArgumentException("Invalid db " + db);
+		}
+	}
+
+	// This is overloading not overriding.
+	public static DBConnection getDBConnection() {
+		return getDBConnection(DEFAULT_DB);
+	}
+}
---
+++ src/external/ExternalAPI.java
@@ -0,0 +1,8 @@
+package external;
+
+import java.util.List;
+import entity.Item;
+
+public interface ExternalAPI {
+	public List<Item> search(double lat, double lon, String term);
+}
